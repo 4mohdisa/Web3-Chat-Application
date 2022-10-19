a28ae56a94b592623a226dd69356d3b1547dbc06
Xcoder <84847782+4mohdisa@users.noreply.github.com>
2022-10-19 20:29:38 +1030

    &apos;

--- components/Messages.js
+++ components/Messages.js
@@ -43,7 +43,7 @@ const Messages = () => {
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
       </div>
       <div ref={endOfMessagesRef} className="text-center text-gray-400 mt-5">
-        <p>You're up to date {user.getUsername()}!</p>
+        <p>You&apos;re up to date {user.getUsername()}!</p>
       </div>
     </div>
   );
